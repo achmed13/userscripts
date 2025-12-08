d694496e79f1b5d2f2f8f8c82a9bbc7ae60891c9
Sean Loos <loossean@gmail.com>
2025-12-08 10:51:57 -0500

    updated publish script

--- publish.bat
+++ publish.bat
@@ -4,5 +4,4 @@ setlocal EnableDelayedExpansion
 rem copy %~dp0*.js H:\web\seanloos.com\userscripts\
 rem copy %~dp0user.css\*.* H:\web\seanloos.com\userscripts\user.css\
 
-wsl rsync -av --info=progress2 --exclude=*.code-workspace --exclude=*.bat /mnt/c/MappingSolutions/development/userscripts/ rocky.seanloos.com:~/stacks/web/www/userscripts/
-pause
+wsl rsync -ah --info=progress2 --exclude=*.code-workspace --exclude=.git --exclude=*.bat --exclude=*.code-workspace /mnt/c/development/userscripts/ rocky:~/stacks/web/www/userscripts/
